4329f2e2294712cb5871c4e57aa29e66eb2c03c8
Luong Chau Tuan <skyatnight.luong@gmail.com>
2015-01-12 10:07:30 +0700

    --Add menu left

---
+++ BasetaxPlus/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  BasetaxPlus
+//
+//  Created by Hung Kiet Ngo on 1/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
